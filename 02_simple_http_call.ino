d09d040316776fa9870ea7f98573188e3dec4724
albertgreinoecker <albert.greinoecker@gmail.com>
2023-04-28 17:01:26 +0200

    Create 02_simple_http_call.ino

---
+++ 02_simple_http_call.ino
@@ -0,0 +1,54 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "YOUR SSID HERE";
+const char* password = "YOUR PASSWORD HERE";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if(WiFi.status()== WL_CONNECTED){
+    String response = httpGETRequest("http://worldtimeapi.org/api/timezone/Europe/London.txt");
+    Serial.print(response);
+    delay(5000);
+  }
+}
+
+/* https://randomnerdtutorials.com/esp32-http-get-open-weather-map-thingspeak-arduino/ */
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
